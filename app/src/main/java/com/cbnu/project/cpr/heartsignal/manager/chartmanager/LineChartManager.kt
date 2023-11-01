076100a0ecca6fec8d6d9dd704392daa0480fb2d
kimtaewan22 <mytae1@naver.com>
2023-11-01 13:54:53 +0900

    refactoring: 그래프 렌더링 에러 수정 및 전체 데이터 그리기

--- app/src/main/java/com/cbnu/project/cpr/heartsignal/manager/chartmanager/LineChartManager.kt
+++ app/src/main/java/com/cbnu/project/cpr/heartsignal/manager/chartmanager/LineChartManager.kt
@@ -1,5 +1,6 @@
 package com.cbnu.project.cpr.heartsignal.manager.chartmanager
 
+import android.R.attr.entries
 import android.graphics.Color
 import android.util.Log
 import com.cbnu.project.cpr.heartsignal.R
@@ -9,14 +10,16 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.EntryXComparator
+import java.util.Collections
+
 
 object LineChartManager {
-
     private lateinit var m_lineChart: LineChart
     private val lineDataSetList = ArrayList<Entry>() // entry_chart1
     private val xValues = mutableListOf<Float>()
-    private val maxDataPoints = 100 // 최대 데이터 포인트 개수
-    private val TotalDataSet = ArrayList<Entry>()
+    private val maxDataPoints = 30 // 최대 데이터 포인트 개수
+    private val totalDataSet = ArrayList<Entry>()
 
 
 
@@ -32,7 +35,7 @@ object LineChartManager {
     fun setupLineChart() {
         Log.d("CAMERAFRAGMENT_addBLEDataToList_setupLineChart", lineDataSetList.toString())
 
-        m_lineChart?.apply {
+        m_lineChart.apply {
             setTouchEnabled(true)
             isClickable = false
             isDoubleTapToZoomEnabled = false
@@ -63,7 +66,7 @@ object LineChartManager {
                     textSize = 12f
                 })
                 axisMinimum = 0f // y-축 최소값 설정
-                axisMaximum = 300f // y-축 최대값 설정
+                axisMaximum = 200f // y-축 최대값 설정
             }
 
             axisRight.apply {
@@ -83,50 +86,50 @@ object LineChartManager {
     }
 
     fun addBLEDataToList(bleData: String) {
-        if(bleData.contains("_"))
-        {
-            val parts = bleData.replace("?","").split("_")
+        if (bleData.contains("_")) {
+            val parts = bleData.replace("?", "").split("_")
             val time = (parts[0].toFloat() / 1000) // 첫 번째 부분
-            val intensity = parts[1].toFloat()
+            val intensity = parts[1].split(",")[0].toFloat()
 
-            xValues.add(time)
-//            Log.d("CAMERAFRAGMENT_addBLEDataToList", xValues.toString())
+            Log.d("CAMERAFRAGMENT_addBLEDataToList_setupLineChartData", "time :${time}, intensity: ${intensity}")
 
-            // x 좌표 값 추가 및 관리
-            if (lineDataSetList.size > maxDataPoints) {
-                // x 좌표 값이 최대 개수를 초과하면 이전 값 제거
-                lineDataSetList.removeAt(0)
-                xValues.removeAt(0)
-                for (i in 0 until lineDataSetList.size) {
-                    if (i < xValues.size) {
-                        lineDataSetList[i].x = xValues[i]
-                    }
-                }
-            }
+//            if (lineDataSetList.size >= maxDataPoints) {
+//                // x 좌표 값이 최대 개수를 초과하면 이전 값 제거
+//                lineDataSetList.removeAt(0)
+//                xValues.removeAt(0)
+//            }
+            totalDataSet.add(Entry(time, intensity))
             lineDataSetList.add(Entry(time, intensity))
-            var lineDataSet = LineDataSet(lineDataSetList, "LineGraph1")
-            // 선의 색상을 보라색으로 변경
-            lineDataSet.color = R.drawable.fade
+            xValues.add(time)
 
-            // 그래프 내부를 색상으로 채우기
+            Collections.sort(lineDataSetList, EntryXComparator())
+
+            var lineDataSet = LineDataSet(lineDataSetList, "LineGraph1")
+            lineDataSet.color = R.drawable.fade
             lineDataSet.setDrawFilled(true)
             lineDataSet.fillColor = R.drawable.fade
-//        lineDataSet1.fillAlpha = 30 // 채우기 색상의 투명도 설정 (0-255 사이의 값)
-            val minX = xValues.minOrNull() ?: 0f
-            val maxX = xValues.maxOrNull() ?: 30f  // 기본값 설정 (원하는 범위로 설정)
-
-//        lineDataSet1.mode = LineDataSet.Mode.LINEAR
-//        lineDataSet1.cubicIntensity = 0.1f
             lineDataSet.setDrawCircleHole(false)
             lineDataSet.setDrawCircles(false)
             lineDataSet.setDrawValues(false)
-            m_lineChart.xAxis?.axisMaximum = maxX
-            m_lineChart.xAxis?.axisMinimum = minX
 
-            var lineData = LineData(lineDataSet)
-            m_lineChart.data = lineData
+            // Ensure LineDataSet is not null
+            if (lineDataSet != null) {
+                // xValues가 비어있지 않은 경우에만 x축 범위를 업데이트합니다.
+                if (xValues.size >= 2) { // 최소 2개의 값을 가지고 있을 때만 첫 번째와 마지막 값을 사용
+                    m_lineChart.xAxis?.axisMinimum = xValues.first()
+                    m_lineChart.xAxis?.axisMaximum = xValues.last()
+                } else {
+                    // xValues가 비어있을 때 또는 한 개의 값만 있을 때 예외 처리
+                    m_lineChart.xAxis?.axisMinimum = 0f
+                    m_lineChart.xAxis?.axisMaximum = 1f
+                }
+
+                val lineData = LineData(lineDataSet)
+                m_lineChart.data = lineData
+            }
         }
     }
+
 
     fun generateInitialData(){
         lineDataSetList.add(Entry(0f,0f))
@@ -151,5 +154,4 @@ object LineChartManager {
     fun setUpTotalData(){
 
     }
-
 }
