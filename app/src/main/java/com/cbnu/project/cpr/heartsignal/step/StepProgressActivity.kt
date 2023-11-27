ee70215144f4e8047cfcce11e4f367380d06c68f
Nikellodian <nikellodian@gmail.com>
2023-11-28 00:52:13 +0900

    feat/스텝 진행마다 하트 애니메이션 추가

--- app/src/main/java/com/cbnu/project/cpr/heartsignal/step/StepProgressActivity.kt
+++ app/src/main/java/com/cbnu/project/cpr/heartsignal/step/StepProgressActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.cbnu.project.cpr.heartsignal.R
+import com.cbnu.project.cpr.heartsignal.adapter.GridHeartAdapter
 import com.cbnu.project.cpr.heartsignal.databinding.ActivityStepProgressBinding
+import com.cbnu.project.cpr.heartsignal.viewModel.BadgeHeartModel
 import com.kofigyan.stateprogressbar.StateProgressBar
+
 
 class StepProgressActivity : AppCompatActivity() {
     private lateinit var binding : ActivityStepProgressBinding
     private lateinit var stateProgressBar: StateProgressBar
+    private lateinit var gridLayout: GridView
     private val DEFAULTVALUE = "FALSE"
 
 
@@ -23,6 +28,7 @@ class StepProgressActivity : AppCompatActivity() {
         binding = ActivityStepProgressBinding.inflate(layoutInflater)
         setContentView(binding.root)
         stateProgressBar = binding.stateProgressBar
+        gridLayout = binding.gridBadge
 
 
         val stepFlag = intent.getStringExtra("stepFlag")
@@ -33,18 +39,20 @@ class StepProgressActivity : AppCompatActivity() {
                 stateProgressBar.foregroundColor = ContextCompat.getColor(this, R.color.colorStateOne)
                 stateProgressBar.stateDescriptionColor = ContextCompat.getColor(this, R.color.colorStateOne)
                 stateProgressBar.currentStateDescriptionColor = ContextCompat.getColor(this, R.color.colorStateOne)
-
                 navigateNext(stepFlag)
             }
             "음성 인식" -> {
+                setUpGridHeart1()
                 stateProgressBar.setStateDescriptionData(descriptionData)
                 stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO)
                 stateProgressBar.foregroundColor = ContextCompat.getColor(this, R.color.colorStateTwo)
                 stateProgressBar.stateDescriptionColor = ContextCompat.getColor(this, R.color.colorStateTwo)
                 stateProgressBar.currentStateDescriptionColor = ContextCompat.getColor(this, R.color.colorStateTwo)
                 navigateNext(stepFlag)
+
             }
             "심폐소생술" -> {
+                setUpGridHeart()
                 stateProgressBar.setStateDescriptionData(descriptionData)
                 stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.THREE)
                 stateProgressBar.foregroundColor = ContextCompat.getColor(this, R.color.colorStateThree)
@@ -53,6 +61,7 @@ class StepProgressActivity : AppCompatActivity() {
                 navigateNext(stepFlag)
             }
             "결과" -> {
+                setUpGridHeart2()
                 stateProgressBar.setStateDescriptionData(descriptionData)
                 stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.FOUR)
                 stateProgressBar.foregroundColor = ContextCompat.getColor(this, R.color.colorStateFour)
@@ -64,6 +73,78 @@ class StepProgressActivity : AppCompatActivity() {
 
             }
         }
+    }
+    private fun setUpGridHeart() {
+        gridLayout = binding.gridBadge
+        val badgeModelArrayList: MutableList<BadgeHeartModel> = mutableListOf<BadgeHeartModel>()
+
+//        for i in [0, 1]
+//            badgeModelArrayList[i] = BadgeHeartModel(R.drawable.heart_image, "")
+
+        //badgeModelArrayList[0] = BadgeHeartModel(R.drawable.w_heart_image, "")
+
+        //step1
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image, ""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image, ""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        //step2
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+
+        val adapter = GridHeartAdapter(this, badgeModelArrayList)
+        gridLayout.adapter = adapter
+    }
+    private fun setUpGridHeart1() {
+        gridLayout = binding.gridBadge
+        val badgeModelArrayList: MutableList<BadgeHeartModel> = mutableListOf<BadgeHeartModel>()
+
+//        for i in [0, 1]
+//            badgeModelArrayList[i] = BadgeHeartModel(R.drawable.heart_image, "")
+
+        //badgeModelArrayList[0] = BadgeHeartModel(R.drawable.w_heart_image, "")
+
+        //step1
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image, ""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image, ""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        //step2
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart2_image,""))
+
+        val adapter = GridHeartAdapter(this, badgeModelArrayList)
+        gridLayout.adapter = adapter
+    }
+    private fun setUpGridHeart2() {
+        gridLayout = binding.gridBadge
+        val badgeModelArrayList: MutableList<BadgeHeartModel> = mutableListOf<BadgeHeartModel>()
+
+//        for i in [0, 1]
+//            badgeModelArrayList[i] = BadgeHeartModel(R.drawable.heart_image, "")
+
+        //badgeModelArrayList[0] = BadgeHeartModel(R.drawable.w_heart_image, "")
+
+        //step1
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image, ""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image, ""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        //step2
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+        badgeModelArrayList.add(BadgeHeartModel(R.drawable.heart_image,""))
+
+        val adapter = GridHeartAdapter(this, badgeModelArrayList)
+        gridLayout.adapter = adapter
     }
 
     private fun navigateNext(stepFlag: String) {
---
+++ app/src/main/java/com/cbnu/project/cpr/heartsignal/viewModel/BadgeHeartModel.kt
@@ -0,0 +1,11 @@
+package com.cbnu.project.cpr.heartsignal.viewModel
+
+class BadgeHeartModel(var imgId: Int, var title: String) {
+    fun getImg(): Int {
+        return imgId
+    }
+
+    fun getBadgeTitle(): String {
+        return title
+    }
+}
